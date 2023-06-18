7fd8d510ae0b89b70c312d2820983282c20f340f
fellix1905 <daydreaming519@naver.com>
2023-06-18 11:44:38 +0900

    startScreen fix

---
+++ ossProject5/UI.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Root.h"
+#include "InGame.h"
+#include "Cursor.h"
+
+
+
+void setFullScreen();
+void alignCenter(const char* str, int posY);
+int printStartTitle();
+int selectStage();
+int isAvailable(int);
+void showRecord();
+
+void showOpeningScreen();
+
+int ResetRecord();
